1053abd77d765a0ceeffe861cfb239a271994122
Muhammad Zubair Shabbir <bsef20a534@pucit.edu.pk>
2023-06-18 03:30:04 +0500

    Database feature

---
+++ app/src/main/java/com/example/madrasaapplication/DbHelper.java
@@ -0,0 +1,45 @@
+package com.example.madrasaapplication;
+
+import android.annotation.SuppressLint;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class DbHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "students.db";
+    private static final String TABLE_NAME = "students";
+
+    private static final String COLUMN_ID = "id";
+    private static final String COLUMN_NAME = "name";
+    private static final String COLUMN_ROLLNO = "roll_no";
+    private static final String COLUMN_ENROLL = "is_enroll";
+
+    public DbHelper(Context context) {
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String sql = "CREATE TABLE IF NOT EXISTS " + TABLE_NAME + "("
+                + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + COLUMN_NAME + " TEXT,"
+                + COLUMN_ROLLNO + " TEXT,"
+                + COLUMN_ENROLL + " INTEGER"
+                + ")";
+        db.execSQL(sql);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        String sql = "DROP TABLE IF EXISTS " + TABLE_NAME;
+        db.execSQL(sql);
+        onCreate(db);
+    }
+}
