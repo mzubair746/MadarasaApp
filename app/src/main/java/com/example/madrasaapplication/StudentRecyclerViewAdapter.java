bccb86c7aa55e6e98722a5534e91e31c78598b1f
Muhammad Zubair Shabbir <bsef20a534@pucit.edu.pk>
2023-06-21 13:59:38 +0500

    StudentRecyclerViewAdapter activity

---
+++ app/src/main/java/com/example/madrasaapplication/StudentRecyclerViewAdapter.java
@@ -0,0 +1,14 @@
+package com.example.madrasaapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StudentRecyclerViewAdapter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_recycler_view_adapter);
+    }
+}
